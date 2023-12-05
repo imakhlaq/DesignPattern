98521df0d9e6af295af259b319f932baa459f14e
imakhlaq <imakhlaqXD@gmail.com>
2023-12-06 03:55:46 +0530

    I in S.O.L.I.D

---
+++ 1 S.O.L.I.D/4 InterfaceSegrrationPrinciple.java
@@ -0,0 +1,55 @@
+/*
+Interface Segregation Principle states that instead of having a big interface with a bunch of methods. Divide them into small interfaces.
+
+A class should not be forced to implement all the methods that it does not require.
+
+So we need ISP for this
+ */
+
+interface IMechanicalCat {
+    void meow();
+}
+
+interface IPetCat extends IMechanicalCat {
+    void drinkMilk();
+}
+
+class MechanicalCatISP implements IMechanicalCat {
+    @Override
+    public void meow() {
+        System.out.println("Meow Meow");
+    }
+}
+
+class PetCatISP implements IPetCat {
+
+    @Override
+    public void meow() {
+        System.out.println("Meow Meow");
+    }
+
+    @Override
+    public void drinkMilk() {
+        System.out.println("Drinking Milk");
+    }
+}
+
+class MainISP {
+    public static void main(String[] args) {
+
+        IMechanicalCat mechanicalCat = new MechanicalCatISP();
+        IPetCat petCat = new PetCatISP();
+
+        petCat.meow();
+        petCat.drinkMilk();
+
+        mechanicalCat.meow();
+
+        /*
+        Here we have separated one interface into multiple interface. So we don't have to implement those methods that are not needed in the class.
+
+
+         */
+
+    }
+}
