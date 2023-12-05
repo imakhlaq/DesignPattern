075fb27780600ea850810797a249e06e6f5d54e6
imakhlaq <imakhlaqXD@gmail.com>
2023-12-05 19:45:16 +0530

    S in S.O.L.I.D

--- 1 S.O.L.I.D/1 SingleResponsiblityPrinciple.java
+++ 1 S.O.L.I.D/1 SingleResponsiblityPrinciple.java
@@ -1,5 +1,5 @@
 /*
-Single Responsiblity Principle means that one class should have one and only one responsibility. And only one actor(for one thing) should use that class.
+Single Responsibility Principle means that one class should have one and only one responsibility. And only one actor(for one thing) should use that class.
 */
 
 class shape {
@@ -9,7 +9,7 @@ class shape {
     }
 
     public String printInString() {
-        return "data in string"
+        return "data in string";
     }
 
 }
@@ -28,20 +28,20 @@ class shapeCalculator {
 
     //breaking SRS
     public String printInJSON() {
-        return "data in string"
+        return "data in string";
     }
 
 }
 /*
 Above ShapeCalculator class have two methods.
 
-printInString(): This method print in JSON. Which is different then doing calculation. And it will be used where u need to print the data in JSON format. So another actor is going to be using this method so u have to define this menthod in andother class.
+printInString(): This method print in JSON. Which is different from doing calculation. And it will be used where u need to print the data in JSON format. So another actor is going to be using this method so u have to define this menthod in andother class.
 
 
 FIX:
 */
 
-class shapeCalculator {
+class ShapeCalculator {
 
     // this method helps to do calculations on shape
     public void calculateArea() {
@@ -53,7 +53,7 @@ class shapePrint{
 
     //Not-breaking SRS
     public String printInJSON() {
-        return "data in string"
+        return "data in string";
     }
 }
 
@@ -77,7 +77,7 @@ class Employee {
 
     }
 
-    public void evoluateEmployee(String employee) {
+    public void evaluateEmployee(String employee) {
 
     }
 }
@@ -87,24 +87,57 @@ Employee class only contains methods related to employee. Yet it is breaking SRS
 
 class Employee{
 
-    private void getEmployeesDetails(String employee){
+    private void getEmployeesDetails(String employee){  => will be used by rest of the method to get details of employee
 
     }
-    public void hireEmployee(String employee){      => used by HR
+    public void hireEmployee(String employee){         => used by HR
 
     }
-    public void getSalary(String employee){         => used by financial team
+    public void getSalary(String employee){            => used by financial team
 
     }
-    public void getStatus(String employee){         =>  used by manager
+    public void getStatus(String employee){            =>  used by manager
 
     }
-    public void evoluateEmployee(String employee){  => used by
+    public void evaluateEmployee(String employee){     => used by manager
 
     }
-
-Even though this employee class have only employees related methods but the actors that are using this class is 4.
-
-
+}
+Even though this employee class have only employees related methods but 4 actors are going to use these method. So multiple actor like HR, Financial team, Manager are going to access these methods so this break the single responsibility principle.
 
 */
+
+// FIX
+
+abstract class EmployeeDetails {
+
+    private void getEmployeesDetails(String employee){ // => will be used by rest of the method to get details of employee
+
+    }
+}
+
+class ManageEmployee extends EmployeeDetails{
+
+    public void getStatus(String employee){          //  =>  used by manager
+
+    }
+    public void evaluateEmployee(String employee){   //  => used by manager
+
+    }
+}
+
+class EmployeeSalary extends EmployeeDetails{
+
+    public void getSalary(String employee){           // => used by financial team
+
+    }
+}
+
+
+class HireEmployee extends EmployeeDetails{
+
+    public void hireEmployee(String employee){       //  => used by HR
+
+
+    }
+}
