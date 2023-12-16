9667919f1236d95520f5f149f57e9e0edc32b5f2
imakhlaq <imakhlaqXD@gmail.com>
2023-12-16 23:50:24 +0530

    factory method desgin pattern

---
+++ 2 Creational Desgin Patterns/02_FactoryPattern/02_MethodFactoryPattern.java
@@ -0,0 +1,69 @@
+interface ICard {
+    String cardType();
+
+    Double maxCredit();
+}
+
+class PlatinumCard implements ICard {
+
+    @Override
+    public String cardType() {
+        return "Platinum Card";
+    }
+
+    @Override
+    public Double maxCredit() {
+        return 50000.40;
+    }
+}
+
+class GoldCard implements ICard {
+
+    @Override
+    public String cardType() {
+        return "Gold Card";
+    }
+
+    @Override
+    public Double maxCredit() {
+        return 10000.22;
+    }
+}
+
+abstract class Factory {
+    protected abstract ICard createFactory();
+
+    public ICard getFactory() {
+        return createFactory();
+    }
+}
+
+class PlatinumCardFactory extends Factory {
+
+    @Override
+    protected ICard createFactory() {
+        return new PlatinumCard();
+    }
+}
+
+class GoldCardFactory extends Factory {
+
+    @Override
+    protected ICard createFactory() {
+        return new GoldCard();
+    }
+}
+
+class MainFactoryMethod {
+    public static void main(String[] args) {
+        var cardType = "gold";
+
+        if (cardType.equalsIgnoreCase("gold")) {
+            ICard card = new PlatinumCardFactory().createFactory();
+            
+        } else if (cardType.equalsIgnoreCase("platinum")) {
+            ICard card = new GoldCardFactory().createFactory();
+
+        }
+    }
+}
