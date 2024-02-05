691ab18c67812ded12f37e08058d18ed281d287d
imakhlaq <imakhlaqXD@gmail.com>
2024-02-06 00:01:16 +0530

    builder pattern complex exp

--- 2 Creational Desgin Patterns/03_BuilderPattern/02_ComplexExBDP.java
+++ 2 Creational Desgin Patterns/03_BuilderPattern/02_ComplexExBDP.java
@@ -33,6 +33,12 @@ class KitchenFurniture extends Furniture {
 }
 
 class LawnFurniture extends Furniture {
+
+    LawnFurniture(String name, Double price) {
+        this.name = name;
+        this.price = price;
+    }
+
     @Override
     public Double getPrice() {
         return this.price;
@@ -85,8 +91,51 @@ class Decor {
     //mandatory attr
     Furniture furniture;
 
-    //optional
+    //optional attr
     Flower flower;
+
+    Decor(DecorBuilder decorBuilder) {
+        this.flower = decorBuilder.flower;
+        this.furniture = decorBuilder.furniture;
+    }
+
+
+    public static class DecorBuilder {
+        //mandatory attr
+        Furniture furniture;
+
+        //optional attr
+        Flower flower;
+
+
+        DecorBuilder(String furniture) {
+
+            if (furniture.equalsIgnoreCase("Kitchen")) {
+                this.furniture = new KitchenFurniture("chair", 22.3);
+            } else {
+                this.furniture = new LawnFurniture("table", 102.3);
+            }
+        }
+
+        public DecorBuilder setFlower(String flower) {
+            if (flower.equalsIgnoreCase("indoor")) {
+                this.flower = new OutdoorFlower("chair", 22.3);
+            } else {
+                this.flower = new IndoorFlower("table", 104.3);
+            }
+
+            return this;
+        }
+
+        //more setters for more optional attr and return this. so u can chain stuff
+
+
+        //builder method that is used to build
+        public Decor build() {
+            return new Decor(this);
+        }
+
+    }
 
 
 }
