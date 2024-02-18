971fa47ef631cbc0fb8d4999561e90ba4101c8b6
imakhlaq <imakhlaqXD@gmail.com>
2024-02-19 00:10:14 +0530

    Strategy Desgin pattern

--- 3 Behavioural Design Patterns/03_StrategyPattern/01_StrategyPattern.java
+++ 3 Behavioural Design Patterns/03_StrategyPattern/01_StrategyPattern.java
@@ -1,3 +1,4 @@
+import javax.management.monitor.Monitor;
 
 // Strategy interface
 interface IPayment {
@@ -10,6 +11,8 @@ interface IPayment {
 class CreditCard implements IPayment {
     @Override
     public boolean payNow(String details) {
+
+        // using details request to the server and transfer the money
         return false;
     }
 }
@@ -18,6 +21,8 @@ class CreditCard implements IPayment {
 class DebitCard implements IPayment {
     @Override
     public boolean payNow(String details) {
+
+        // using details request to the server and transfer the money
         return false;
     }
 }
@@ -30,16 +35,70 @@ class NetBanking implements IPayment {
     }
 }
 
+//Strategy #3
+class UPI implements IPayment {
+    @Override
+    public boolean payNow(String details) {
+        return true;
+    }
+}
+
 //Context
 class ProductC {
 
     String name;
-    String price;
+    Double price;
+
+    ProductC(String name, Double price) {
+        this.name = name;
+        this.price = price;
+    }
+}
+
+class Order {
+
+    ProductC product;
+    IPayment payment;
+
+    Order(ProductC product, IPayment payment) {
+        this.payment = payment;
+        this.product = product;
+    }
+
+    public String orderNow(String orderDetails) {
+
+        if (payment.payNow(orderDetails)) return "order is places for " + product.name;
+
+        return "Order is unsuccessful for " + product.name;
+    }
 }
 
 //client
 class StrategyMain {
     public static void main(String[] args) {
+
+        // customer want to buy this product
+        var monitor = new ProductC("LG UltraGear", 25000.0);
+
+        // customer a payment method
+        var chosenMethod = "UPI";
+        if (chosenMethod.equalsIgnoreCase("creditCard")) {
+
+            var order = new Order(monitor, new CreditCard());
+            // card number
+            var orderStatus = order.orderNow("91231488131331");
+
+            System.out.println(orderStatus);
+        }
+
+        if (chosenMethod.equalsIgnoreCase("upi")) {
+
+            var order = new Order(monitor, new UPI());
+            // card number
+            var orderStatus = order.orderNow("imakhlaq@upi");
+
+            System.out.println(orderStatus);
+        }
 
     }
 }
