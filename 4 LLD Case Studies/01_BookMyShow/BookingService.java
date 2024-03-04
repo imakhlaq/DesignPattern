ddb6245c59c893511ac7954241035d1c6e6c813d
imakhlaq <imakhlaqXD@gmail.com>
2024-03-04 23:30:46 +0530

    bookmyshow

---
+++ 4 LLD Case Studies/01_BookMyShow/BookingService.java
@@ -0,0 +1,8 @@
+import java.time.LocalDateTime;
+
+public class BookingService implements IBookingService {
+    @Override
+    public Long createBooking(Long userId, Long movie, LocalDateTime showTime, Integer noOfSeats) {
+        return null;
+    }
+}
